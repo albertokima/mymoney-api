b6ded3bef81ec1759d3608d42ebacf785a04256f
MasterRyo <betolk@msn.com>
2018-03-23 17:45:51 -0300

    Atualizacao sqls

--- src/main/resources/db/migration/V001__criar_e_registrar_categorias.sql
+++ src/main/resources/db/migration/V001__criar_e_registrar_categorias.sql
@@ -5,8 +5,18 @@ CREATE TABLE categoria
   CONSTRAINT categoria_pkey PRIMARY KEY (codigo)
 );
 
-INSERT INTO categoria (nome) values ('Lazer');
-INSERT INTO categoria (nome) values ('Alimentação');
-INSERT INTO categoria (nome) values ('Supermercado');
-INSERT INTO categoria (nome) values ('Farmácia');
-INSERT INTO categoria (nome) values ('Outros');
+INSERT INTO categoria VALUES (1, 'Lazer');
+INSERT INTO categoria VALUES (2, 'Alimentação');
+INSERT INTO categoria VALUES (3, 'Supermercado');
+INSERT INTO categoria VALUES (4, 'Farmácia');
+INSERT INTO categoria VALUES (6, 'Financiamento');
+INSERT INTO categoria VALUES (8, 'Impostos');
+INSERT INTO categoria VALUES (9, 'Despesas Bancárias');
+INSERT INTO categoria VALUES (10, 'Restaurantes');
+INSERT INTO categoria VALUES (12, 'Informática');
+INSERT INTO categoria VALUES (13, 'Clientes');
+INSERT INTO categoria VALUES (14, 'Vendas');
+INSERT INTO categoria VALUES (5, 'Transporte');
+INSERT INTO categoria VALUES (7, 'Receitas');
+INSERT INTO categoria VALUES (15, 'Salários');
+
--- src/main/resources/db/migration/V002__criar_e_registrar_pessoas.sql
+++ src/main/resources/db/migration/V002__criar_e_registrar_pessoas.sql
@@ -14,3 +14,7 @@ CREATE TABLE pessoa
 );
 
 INSERT INTO pessoa (nome, ativo, logradouro, numero, bairro, cep, cidade, estado) values ('Alberto','true','Av Gaturamo','320','Jd Primavera','86702-001','Arapongas','PR');
+INSERT INTO pessoa VALUES (3, 'Eder Yvan Gomes', 'Rua Osório Ribas de Paula', '794', NULL, 'Centro', '86.800-140', 'Apucarana', 'PR', false);
+INSERT INTO pessoa VALUES (6, 'Comercial', 'Av Arapongas', '1000', 'Sala 401', 'Centro', '86.800-002', 'Arapongas', 'PR', true);
+INSERT INTO pessoa VALUES (2, 'Charles', 'Av Curitiba', '1234', 'de 1122/1123 ao fim', 'Centro', '86.800-704', 'Apucarana', 'PR', false);
+
